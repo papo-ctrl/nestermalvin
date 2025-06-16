9a271cef6081decba268dc4fafe6d1d9ad495d4f
papo-ctrl <damojcik@gmail.com>
2025-06-16 17:06:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pyarmor clean
-pyarmor pack -e " --onefile" -x " --exclude tests" .
+
+# Удаляем старую папку, если была
+rm -rf dist
+
+# Создаём новую папку
+mkdir dist
+
+# Генерим зашифрованный бот в папку dist
+pyarmor gen -O dist nestermalvin.py
